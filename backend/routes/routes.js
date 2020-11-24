2af4ebf087896afca6b2a80ef96be1a2db30684a
Your Name <you@example.com>
2020-11-23 19:30:04 -0500

    jmmm

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -38,7 +38,6 @@ router.post('/login/',(req,res)=>{
 
 
 router.get('/clientes', (req, res) => {
-     
   mysqlConnection.query('SELECT * FROM usuario ', (err, rows, fields) => {
       if (!err) {
         res.json(rows);
@@ -47,18 +46,17 @@ router.get('/clientes', (req, res) => {
       }
     });
   });
+
+ router.get('/clientes/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM usuario WHERE id_usuario =? ', (err, rows, fields) => {
+        if (!err) {
+          res.json(rows);
+        } else {
+          console.log(err);
+        }
+      });
+    });
   
-router.get('/usuario/:id', (req, res) => {
-  const { id } = req.params;
-  mysqlConnection.query('SELECT * FROM usuario WHERE id_usuario = ?',
-   [id], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'Cliente visto ahora mismo!'});
-    } else {
-      console.log(err);
-    }
-  });
-});
 
 router.put('/cambiocontraseña/:id', (req, res) => {
   const {contraseña} = req.body;
