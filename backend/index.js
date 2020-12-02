cd9250a20b4fe40a47e3ce547f5fb1e5f05cbb31
Your Name <you@example.com>
2020-12-02 14:10:51 -0500

    mmmmmm

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const cors = require('cors')
-
-app.use(cors({origin: '*'}));
-
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
-app.get('/',(req,res)=>{
-  res.send('hi');
-});
+
 // Ajustes
 app.set('port',process.env.PORT || 3001);
 
 // Middlewares
 app.use(express.json());
-
+app.get('/',(req,res)=>{
+  res.send('hi');
+});
 // Routes//
 app.use('/api',routes);
-
 // Ajustes del servidor
 app.listen(app.get('port'), () => {
   console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
