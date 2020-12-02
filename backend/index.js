f94f99d26b062809bc12813b08c020f7b7c1b8e7
Your Name <you@example.com>
2020-12-02 14:03:18 -0500

    mmmmm

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
-const cors = require('cors');
 const bodyParser = require('body-parser');
+const cors = require('cors')
+
+app.use(cors({origin: '*'}));
 
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.get('/',(req,res)=>{
+  res.send('hi');
+});
 // Ajustes
 app.set('port',process.env.PORT || 3001);
 
 // Middlewares
 app.use(express.json());
-app.use(cors({origin: '*'}));
-
-app.get('/',(req,res)=>{
-  res.send('hi');
-});
 
 // Routes//
 app.use('/api',routes);
